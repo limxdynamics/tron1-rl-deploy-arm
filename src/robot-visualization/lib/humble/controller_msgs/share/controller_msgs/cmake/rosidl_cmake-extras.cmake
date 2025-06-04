081d39a299097e3e47d5cea639c1da8e399091a0
jenkins_ci <jenkins_ci@limxdynamics.com>
2025-06-04 15:55:27 +0800

    3.0.17.20250604155351

--- src/robot-visualization/lib/humble/controller_msgs/share/controller_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(controller_msgs_IDL_FILES "msg/JointCmd.idl;msg/JointState.idl;msg/IMUData.idl")
-set(controller_msgs_INTERFACE_FILES "msg/JointCmd.msg;msg/JointState.msg;msg/IMUData.msg")
